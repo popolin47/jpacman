f39118a14533ab638987bd57bf27437c46615e25
Praewa Chan-on <107537670+popolin47@users.noreply.github.com>
2025-11-10 17:08:11 +0700

    Update MapParserTest.java

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -53,21 +55,24 @@ public class MapParserTest {
      */
     @Test
     public void testParseMapWrong1() {
-        ADDYOURCODEHERE thrown =
-            Assertions.assertThrows(ADDYOURCODEHERE.class, () -> {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
                 MockitoAnnotations.initMocks(this);
                 assertNotNull(boardFactory);
                 assertNotNull(levelFactory);
                 MapParser mapParser = new MapParser(levelFactory, boardFactory);
                 ArrayList<String> map = new ArrayList<>();
-                /*
-                Create a map with inconsistent size between
-                each row or contain invalid characters
-                */
+            /*
+             Create a map with inconsistent size between
+             each row or contain invalid characters
+             */
+                map.add("#####");
+                map.add("###"); // shorter row → invalid width
                 mapParser.parseMap(map);
             });
-        Assertions.assertEquals(ADDYOURCODEHERE, thrown.getMessage());
+        Assertions.assertEquals("Input text lines are not of equal width.", thrown.getMessage());
     }
+
 
 
 
